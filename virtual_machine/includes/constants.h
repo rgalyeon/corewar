41a7894a72fdc06135b907a1ad9216e929a1ee2d
rgalyeon <dmifilin@gmail.com>
2020-07-09 19:14:49 +0300

    Update: vm main header

---
+++ virtual_machine/includes/constants.h
@@ -0,0 +1,27 @@
+
+
+#ifndef CONSTANTS_H
+# define CONSTANTS_H
+
+# define REG_CODE				1
+# define DIR_CODE				2
+# define IND_CODE				3
+
+# define MAX_PLAYERS			4
+
+# define MEM_SIZE				(4 * 1024)
+# define IDX_MOD				(MEM_SIZE / 8)
+# define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
+
+# define REG_NUMBER				16
+
+# define CYCLE_TO_DIE			1536
+# define CYCLE_DELTA			50
+# define NBR_LIVE				21
+# define MAX_CHECKS				10
+
+# define PROG_NAME_LENGTH		128
+# define COMMENT_LENGTH			2048
+# define COREWAR_EXEC_MAGIC		0xea83f3
+
+#endif
--- virtual_machine/includes/virtual_machine.h
+++ virtual_machine/includes/virtual_machine.h
@@ -2,7 +2,30 @@
 
 #ifndef VIRTUAL_MACHINE_H
 # define VIRTUAL_MACHINE_H
+# include <stdbool.h>
+# include "constants.h"
 
 # define UNUSED (void)
+
+/*
+** Playground structure
+*/
+
+typedef struct	s_area
+{
+	char	*map;
+}				t_area;
+
+/*
+** Carriage structure
+*/
+
+typedef struct	s_carriage
+{
+	unsigned int pc;
+	int 		reg[REG_NUMBER];
+	bool		carry;
+
+}				t_carr;
 
 #endif
